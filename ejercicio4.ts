f98239dc3befdd740b5a8ab8f223136568958a6f
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2023-11-02 16:21:07 -0500

    .

--- ejercicio4.ts
+++ ejercicio4.ts
@@ -1,13 +1,32 @@
-interface BaseObject{
-    id:number;
-
-}
-class user implements BaseObject{
-id:number;
-}
-class product implements BaseObject{
-    id:number;
-}
-class order implements BaseObject{
-    id:number;
-}
+interface BaseObject {
+    id: number;
+  }
+  
+  class User implements BaseObject {
+    id: number;
+  
+    constructor(id: number) {
+      this.id = id;
+    }
+  }
+  
+  class Product implements BaseObject {
+    id: number;
+  
+    constructor(id: number) {
+      this.id = id;
+    }
+  }
+  
+  class Order implements BaseObject {
+    id: number;
+  
+    constructor(id: number) {
+      this.id = id;
+    }
+  }
+  
+  function printData<T extends BaseObject>(baseObject: T): void {
+    console.log(baseObject);
+  }
+  
