8fe08b6f71a6e5af748907f1a8a2a8b3467530dc
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2023-11-02 16:24:34 -0500

    ejercicio5

---
+++ ejercicio5.ts
@@ -0,0 +1,24 @@
+interface Database {
+  connect(): void;
+  find(query: string): any[];
+  update(query: string, data: any): void;
+}
+class MySQLDatabase implements Database {
+  connect(): void {}
+
+  find(query: string): any[] {
+    return [];
+  }
+
+  update(query: string, data: any): void {}
+}
+
+class SQLiteDatabase implements Database {
+  connect(): void {}
+
+  find(query: string): any[] {
+    return [];
+  }
+
+  update(query: string, data: any): void {}
+}
